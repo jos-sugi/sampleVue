7841e54301ecdda07d34cab0091271e9ed8da03d
杉澤ほせ <h.s.miruku.1121@gmail.com>
2022-02-14 17:32:41 +0900

    API/axios

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,12 @@
 <template>
   <v-app>
-    <v-main>
-      <v-btn @click="sampleApi">一覧を取得</v-btn>
-    </v-main>
+    <Properties></Properties>
   </v-app>
 </template>
-
 <script>
-import axios from "axios";
+import Properties from "./components/Properties.vue";
 
 export default {
-  name: "App",
-  data() {
-    return {
-      name: "aaa",
-      commnet: "bbb",
-    };
-  },
-  methods: {
-    sampleApi() {
-      axios
-        .post(
-          "https://firestore.googleapis.com/v1/projects/vuejs-sample-ff968/databases/(default)/documents/cities/LA",
-          {
-            fields: {
-              name: {
-                // stringValue: this.name,
-                stringValue: "aaa",
-              },
-              commnent: {
-                // stringValue: this.comment,
-                stringValue: "bbb",
-              },
-            },
-          }
-        )
-        .then((response) => {
-          console.log(response);
-        });
-    },
-  },
+  components: { Properties },
 };
 </script>
--- src/components/Properties.vue
+++ src/components/Properties.vue
@@ -1,80 +1,110 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
+  <v-app>
+    <v-main>
+      <v-container class="mt-15">
         <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
+          <ul v-for="(list, index) in propertyList" :key="index">
+            <li>
+              <v-btn @click="getProperty(list)">{{ list }}</v-btn>
+            </li>
+          </ul>
         </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
         <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
+          <v-col style="text-align: center">
+            <v-btn @click="registerPropaty">登録</v-btn>
+          </v-col>
+          <v-col style="text-align: center">
+            <v-btn @click="updatePropaty">更新</v-btn>
+          </v-col>
         </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "HelloWorld",
+  name: "App",
+  data() {
+    return {
+      name: "aaa",
+      commnet: "bbb",
+      baseUrl: "https://virtserver.swaggerhub.com/jos.sugi03/Property/1.0.0/",
+      propertyList: [],
+      //物件情報
+      prorerty_id: "",
+      size: "",
+      rent: "",
+      age: "",
+      floor: "",
+    };
+  },
+  created() {
+    this.getProperties();
+  },
+  methods: {
+    /**
+     * 物件一覧
+     */
+    getProperties() {
+      var url = this.baseUrl + "properties";
+
+      axios.get(url).then((response) => {
+        this.propertyList = response.data;
+        console.log(this.propertyList);
+      });
+    },
+    /**
+     * 物件詳細
+     */
+    getProperty(value) {
+      var url = this.baseUrl + "properties/" + value.prorerty_id;
+
+      axios.post(url).then((response) => {
+        console.log(response);
+      });
+    },
+    /**
+     * 物件登録
+     */
+    registerPropaty() {
+      var url = this.baseUrl + "properties";
+
+      axios
+        .post(url, {
+          fields: {
+            size: this.size,
+            rent: this.rent,
+            age: this.rent,
+            floor: this.floor,
+          },
+        })
+        .then((response) => {
+          console.log(response);
+        });
+    },
+    /**
+     * 物件更新
+     */
+    updatePropaty() {
+      var url = this.baseUrl + "properties";
+
+      axios
+        .post(url, {
+          fields: {
+            prorerty_id: this.property_id,
+            size: this.size,
+            rent: this.rent,
+            age: this.rent,
+            floor: this.floor,
+          },
+        })
+        .then((response) => {
+          console.log(response);
+        });
+    },
+  },
 };
 </script>
